2c587e2d2a1be614eff3ce6dd7d07236e5edc400
teyvi <44750503+teyvi@users.noreply.github.com>
2024-12-10 12:21:33 +0000

    created layout for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomeLayout from './layouts/HomeLayout';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<HomeLayout/>}>
+      <Route index element={<Home/>}/>
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/layouts/HomeLayout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import SideBar from '../components/SideBar'
+
+
+
+function HomeLayout() {
+  return (
+   <>
+<div className='layout-container'>
+  <SideBar/>
+<main className='main-content'>
+</main>
+</div>
+   </>
+  )
+}
+
+export default HomeLayout
