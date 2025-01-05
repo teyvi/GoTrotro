3f1e0d22c40b76563cfec6cc577e451e8ecaba2f
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-05 18:12:39 +0000

    remove unused Home.css import from HomePage to streamline styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import MapComponent from "../components/MapComponent";
 import SearchComponent from "../components/SearchComponent";
 import HomeLayout from "../layouts/HomeLayout";
-import '../styles/Home.css'
 
 function Home() {
   return (
