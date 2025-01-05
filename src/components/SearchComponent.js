2e88dbd751ca71d8ff053d30a7b1a4f83350adc5
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-05 15:06:15 +0000

    refactor SearchComponent for improved layout and styling

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,32 +1,23 @@
 // SearchComponent.js
-import React from 'react'
-import '../styles/SearchBar.css'
-import { FaLocationDot } from "react-icons/fa6";
-import { FaRegCircle } from 'react-icons/fa';
+import React from 'react';
+import { FaLocationDot, FaRegCircle } from "react-icons/fa6";
 
 function SearchComponent() {
   return (
-    <div>
-      
-      <main className='search'>
-        <div className='search-box'>
-          <div className='search-fields'>
-            <div className='origin-location'>
-              <span><FaRegCircle className="origin-icon" /></span>
-              Select Origin
-            </div>
-            <div className='destination-location'>
-              <span><FaLocationDot className="destination-icon" /></span>
-              Select Destination
-            </div>
-            <div className='swap-locations'>
-
-            </div>
-          </div>
+    <div className="w-full">
+      <div className="flex items-center gap-4 p-2 md:p-3">
+        <div className="flex-1 flex items-center gap-3 p-2 border-2 border-[#AECACD] rounded hover:bg-gray-50 cursor-pointer">
+          <FaRegCircle className="text-blue-500 text-lg shrink-0" />
+          <span className="text-gray-600 truncate">Select Origin</span>
         </div>
-      </main>
+        
+        <div className="flex-1 flex items-center gap-3 p-2 border-2 border-[#AECACD] rounded hover:bg-gray-50 cursor-pointer">
+          <FaLocationDot className="text-red-500 text-lg shrink-0" />
+          <span className="text-gray-600 truncate">Select Destination</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchComponent
+export default SearchComponent;
