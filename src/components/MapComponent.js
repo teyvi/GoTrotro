eaa8fbb1af9ec49c95097a657a1a6ceb74e72dd5
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-20 16:52:54 +0000

    feat: configure MapComponent to use environment variable for MapTiler API key

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -12,8 +12,20 @@ function MapComponent() {
     const map = useRef(null);
     const accra = { lng: 5.614818, lat: -0.205874 };
     const zoom = 14;
-    maptilersdk.config.apiKey = 'YOUR_MAPTILER_API_KEY_HERE';
+    maptilersdk.config.apiKey = process.env.REACT_APP_MAPTILER_API_KEY;
  
+
+    useEffect(() => {
+      if (map.current) return; // stops map from intializing more than once
+    
+      map.current = new maptilersdk.Map({
+        container: mapContainer.current,
+        style: maptilersdk.MapStyle.STREETS,
+        center: [accra.lng, accra.lat],
+        zoom: zoom
+      });
+    
+    }, [accra.lng, accra.lat, zoom]);
 
   return (
     <Map
