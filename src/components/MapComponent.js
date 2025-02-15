16df5adf49647d0c04afd5f91a7a15acd0fe357f
teyvi <44750503+teyvi@users.noreply.github.com>
2025-02-15 19:48:22 +0000

    feat: refactor MapComponent to use react-map-gl for improved performance and cleaner code

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,34 +1,17 @@
-import "maplibre-gl/dist/maplibre-gl.css";
-import React, { useRef, useEffect } from 'react';
-import * as maptilersdk from '@maptiler/sdk';
-import "@maptiler/sdk/dist/maptiler-sdk.css";
-import "../styles/map.css";
+import * as React from 'react';
+import {Map} from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 function MapComponent() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const accra = { lng:-0.205874 , lat:  5.614818};
-    const zoom = 11;
-    maptilersdk.config.apiKey = process.env.REACT_APP_MAPTILER_TOKEN;
- 
-
-    useEffect(() => {
-      if (map.current) return; 
-    
-      map.current = new maptilersdk.Map({
-        container: mapContainer.current,
-        style: maptilersdk.MapStyle.STREETS,
-        center: [accra.lng, accra.lat],
-        zoom: zoom
-      });
-    
-    }, [accra.lng, accra.lat, zoom]);
-
   return (
-
-    <div className="map-wrap">
-    <div ref={mapContainer} className="map" />
-  </div>
+    <Map
+      initialViewState={{
+        longitude: -0.205874,
+        latitude: 5.614818,
+        zoom: 11
+      }}
+      style={{width: '100%', height: `calc(100vh - 20px)`}}
+      mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${process.env.REACT_APP_MAPTILER_TOKEN}`}    />
   );
 }
 
