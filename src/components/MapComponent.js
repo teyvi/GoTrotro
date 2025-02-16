0a8a588965bea2bbe9ec7dbfdbac4bff78d75ea4
teyvi <44750503+teyvi@users.noreply.github.com>
2025-02-16 09:38:14 +0000

    feat: enhance MapComponent with navigation and geolocation controls for improved user experience

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,6 +1,11 @@
-import * as React from 'react';
-import {Map} from 'react-map-gl/maplibre';
-import 'maplibre-gl/dist/maplibre-gl.css';
+import * as React from "react";
+import {
+  GeolocateControl,
+  Map,
+  NavigationControl,
+} from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "../styles/map.css";
 
 function MapComponent() {
   return (
@@ -8,10 +13,23 @@ function MapComponent() {
       initialViewState={{
         longitude: -0.205874,
         latitude: 5.614818,
-        zoom: 11
+        zoom: 11,
       }}
-      style={{width: '100%', height: `calc(100vh - 20px)`}}
-      mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${process.env.REACT_APP_MAPTILER_TOKEN}`}    />
+      style={{
+        position: "absolute",
+        width: "100%",
+        height: "calc(100vh - 20px)",
+      }}
+      mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${process.env.REACT_APP_MAPTILER_TOKEN}`}
+    >
+      <NavigationControl position="bottom-right" />
+      <GeolocateControl
+        position="bottom-right"
+        positionOptions={{ enableHighAccuracy: true }}
+        trackUserLocation={true}
+        showUserHeading={true}
+      />
+    </Map>
   );
 }
 
