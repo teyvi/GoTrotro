1c9583e09a2445badd06d32f1caed85aa7f8731c
teyvi <teyviangela@yahoo.com>
2025-11-26 23:49:32 +0000

    refactor: remove HomePage component and update routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Route, Routes as RouterRoutes } from "react-router-dom";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Home from "./pages/HomePage";
-import React from "react";
+ import React from "react";
 import SingleRoutes from "./pages/Routes";
 import RouteDetails from "./pages/RouteDetails";
 
@@ -13,8 +12,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <RouterRoutes>
-          <Route path="/" element={<Home />} />
-          <Route path="/routes" element={<SingleRoutes />} />
+           <Route path="/" element={<SingleRoutes />} />
           <Route path="/route/:id" element={<RouteDetails />} />
         </RouterRoutes>
       </BrowserRouter>
