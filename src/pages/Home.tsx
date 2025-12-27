5c95e91e08bd52b5ec3256d376377cae89cc7b00
teyvi <teyviangela@yahoo.com>
2025-12-27 12:18:38 +0000

    refactor: remove unused async input change handlers and streamline state management

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,27 +32,8 @@ const SingleRoutes = () => {
     LocationResult[]
   >([]);
   const [routeOptions, setRouteOptions] = useState<RouteOption[]>([]);
-
-  // const handleOriginInputChange = async (
-  //   e: React.ChangeEvent<HTMLInputElement>
-  // ) => {
-  //   const newQuery = e.target.value;
-  //   setOrigin(newQuery);
-  //   setOriginSuggestions([]);
-
-  //   if (newQuery.length > 2) {
-  //     setIsSearchingOrigin(true);
-  //     try {
-  //       const results = await searchLocations(newQuery);
-  //       setOriginSuggestions(results);
-  //       setOriginResults(results);
-  //     } catch (error) {
-  //       console.error("Error fetching origin suggestions:", error);
-  //     } finally {
-  //       setIsSearchingOrigin(false);
-  //     }
-  //   }
-  // };
+  const debouncedOrigin = useDebounce(origin)
+  const debounceDestination = useDebounce(destination)
 
 const handleOriginInputChange = (e:React.ChangeEvent<HTMLInputElement> ) => {
 setOrigin(e.target.value);
@@ -63,27 +44,6 @@ const handleDestinationInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
   setDestination(e.target.value);
   setDestinationSuggestions([]);
 };
-
-  // const handleDestinationInputChange = async (
-  //   e: React.ChangeEvent<HTMLInputElement>
-  // ) => {
-  //   const newQuery = e.target.value;
-  //   setDestination(newQuery);
-  //   setDestinationSuggestions([]);
-
-  //   if (newQuery.length > 2) {
-  //     setIsSearchingDestination(true);
-  //     try {
-  //       const results = await searchLocations(newQuery);
-  //       setDestinationSuggestions(results);
-  //       setDestinationResults(results);
-  //     } catch (error) {
-  //       console.error("Error fetching destination suggestions:", error);
-  //     } finally {
-  //       setIsSearchingDestination(false);
-  //     }
-  //   }
-  // };
 
   const handleSelectSuggestion = (
     location: LocationResult,
