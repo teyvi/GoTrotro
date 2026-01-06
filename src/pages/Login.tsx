f1a88d898e332a618177fbfe1c873a11f7445953
teyvi <teyviangela@yahoo.com>
2026-01-06 19:20:48 +0000

    removed unsed components

--- src/pages/Login.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AuthForm from '../components/AuthForm';
-
-function Login() {
-  return <AuthForm type="login" />;
-}
-
-export default Login;
--- src/pages/Register.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AuthForm from '../components/AuthForm';
-
-function Register() {
-  return <AuthForm type="register" />;
-}
-
-export default Register;
